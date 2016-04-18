4238a07b9ad021ec8af4331d9279ea7f74410523
DGS-Fox <mcfracshun@gmail.com>
2016-04-18 01:37:34 -0500

    message

---
+++ Dungeons and Dragons Character Sheet Creator/Dungeons and Dragons Character Sheet Creator/frmMain.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboRace = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboClass = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'cboRace
+        '
+        Me.cboRace.FormattingEnabled = True
+        Me.cboRace.Location = New System.Drawing.Point(12, 32)
+        Me.cboRace.Name = "cboRace"
+        Me.cboRace.Size = New System.Drawing.Size(121, 24)
+        Me.cboRace.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Choose a Race:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(143, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Choose a Class:"
+        '
+        'cboClass
+        '
+        Me.cboClass.FormattingEnabled = True
+        Me.cboClass.Location = New System.Drawing.Point(143, 32)
+        Me.cboClass.Name = "cboClass"
+        Me.cboClass.Size = New System.Drawing.Size(121, 24)
+        Me.cboClass.TabIndex = 2
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(560, 375)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cboClass)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboRace)
+        Me.Name = "frmMain"
+        Me.Text = "D&D Character Sheet Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cboRace As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboClass As ComboBox
+End Class
