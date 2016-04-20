11423f001811cf1f815dca4edda6f819e1509787
DGS-Fox <mcfracshun@gmail.com>
2016-04-20 11:54:07 -0500

    Updated form to display rolls

--- Dungeons and Dragons Character Sheet Creator/Dungeons and Dragons Character Sheet Creator/clsScoreRoller.vb
+++ Dungeons and Dragons Character Sheet Creator/Dungeons and Dragons Character Sheet Creator/clsScoreRoller.vb
@@ -1,11 +1,13 @@
 ﻿Public Class clsScoreRoller
 #Region "Const and Variables"
-    Protected RandomNumberGenerator As New Random(DateAndTime.Now.Millisecond)
+    Const MAX_ROLL As Integer = 7 'need to be 7, because 6 wont hit
+    Const LOW_ROLL As Integer = 1
     'Declare Variables for the stat
-    Private Roll1, Roll2, Roll3, Roll4, LowestNumber, Score As Integer
+    Private Roll1, Roll2, Roll3, Roll4, LowestNumber, Score, seed As Integer
 #End Region
 #Region "Constructors"
-    Sub New()
+    Sub New(ByVal NewSeed As Integer)
+        seed = NewSeed
         Generate()
     End Sub
 #End Region
@@ -40,22 +42,56 @@
             Return LowestNumber
         End Get
     End Property
+    ReadOnly Property getModifer
+        Get
+            Select Case Score
+                Case 18
+                    Return 4
+                Case 17, 16
+                    Return 3
+                Case 15, 14
+                    Return 2
+                Case 13, 12
+                    Return 1
+                Case 11, 10
+                    Return 0
+                Case 9, 8
+                    Return -1
+                Case 7, 6
+                    Return -2
+                Case 5, 4
+                    Return -3
+                Case 3
+                    Return -4
+                Case Else
+                    Return "FAILURE"
+                    MessageBox.Show("Error in score roller.")
+            End Select
+        End Get
+    End Property
 #End Region
 #Region "Methods"
+
     Private Sub Generate()
+        Dim RandomNumberGenerator As New Random(seed)
         'Create an array for the scores. I know you dont like arrays so...fuck you i dont care
         Dim scoreArr(3) As Integer
         'Set Roll Values
-        scoreArr(0) = Roll1 = RandomNumberGenerator.Next(1, 6)
-        scoreArr(1) = Roll2 = RandomNumberGenerator.Next(1, 6)
-        scoreArr(2) = Roll3 = RandomNumberGenerator.Next(1, 6)
-        scoreArr(3) = Roll4 = RandomNumberGenerator.Next(1, 6)
+        Roll1 = RandomNumberGenerator.Next(LOW_ROLL, MAX_ROLL)
+        Roll2 = RandomNumberGenerator.Next(LOW_ROLL, MAX_ROLL)
+        Roll3 = RandomNumberGenerator.Next(LOW_ROLL, MAX_ROLL)
+        Roll4 = RandomNumberGenerator.Next(LOW_ROLL, MAX_ROLL)
+        'place them in the array
+        scoreArr(0) = Roll1
+        scoreArr(1) = Roll2
+        scoreArr(2) = Roll3
+        scoreArr(3) = Roll4
         'Add All Scores Together
         Score = Roll1 + Roll2 + Roll3 + Roll4
         'Now Sift through and find the lowest Value
         Array.Sort(scoreArr)
         'Get the lowest number
-        LowestNumber = scoreArr(3)
+        LowestNumber = scoreArr(0)
         'remove the lowest number from the total
         Score -= LowestNumber
     End Sub
--- Dungeons and Dragons Character Sheet Creator/Dungeons and Dragons Character Sheet Creator/frmMain.vb
+++ Dungeons and Dragons Character Sheet Creator/Dungeons and Dragons Character Sheet Creator/frmMain.vb
@@ -1,4 +1,6 @@
 ﻿Public Class frmMain
+    Dim RandomNumberGenerator As New Random(DateAndTime.Now.Millisecond)
+
 #Region "Functions and Subs"
     Private Sub EnableControls()
         cboClass.Enabled = True
@@ -8,7 +10,82 @@
 
 
     Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
-        MessageBox.Show("generate character function")
+
+        Dim Roller1 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim Roller2 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim Roller3 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim Roller4 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim Roller5 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim Roller6 As New clsScoreRoller(CInt(RandomNumberGenerator.Next(1, 100)))
+        Dim TotalModCheck As Integer = 0
+
+        lblRNG1.Text = Roller1.getScore.ToString
+        lblRNG2.Text = Roller2.getScore.ToString
+        lblRNG3.Text = Roller3.getScore.ToString
+        lblRNG4.Text = Roller4.getScore.ToString
+        lblRNG5.Text = Roller5.getScore.ToString
+        lblRNG6.Text = Roller6.getScore.ToString
+
+        'This chunk is to display values for testing
+        stat1roll1.Text = Roller1.getRoll1.ToString
+        stat1roll2.Text = Roller1.getRoll2.ToString
+        stat1roll3.Text = Roller1.getRoll3.ToString
+        stat1roll4.Text = Roller1.getRoll4.ToString
+        stat1low.Text = Roller1.getLowestNumber.ToString
+        stat1total.Text = Roller1.getScore.ToString
+        stat1mod.Text = Roller1.getModifer.ToString
+
+        stat2roll1.Text = Roller2.getRoll1.ToString
+        stat2roll2.Text = Roller2.getRoll2.ToString
+        stat2roll3.Text = Roller2.getRoll3.ToString
+        stat2roll4.Text = Roller2.getRoll4.ToString
+        stat2low.Text = Roller2.getLowestNumber.ToString
+        stat2total.Text = Roller2.getScore.ToString
+        stat2mod.Text = Roller2.getModifer.ToString
+
+        stat3roll1.Text = Roller3.getRoll1.ToString
+        stat3roll2.Text = Roller3.getRoll2.ToString
+        stat3roll3.Text = Roller3.getRoll3.ToString
+        stat3roll4.Text = Roller3.getRoll4.ToString
+        stat3low.Text = Roller3.getLowestNumber.ToString
+        stat3total.Text = Roller3.getScore.ToString
+        stat3mod.Text = Roller3.getModifer.ToString
+
+        stat4roll1.Text = Roller4.getRoll1.ToString
+        stat4roll2.Text = Roller4.getRoll2.ToString
+        stat4roll3.Text = Roller4.getRoll3.ToString
+        stat4roll4.Text = Roller4.getRoll4.ToString
+        stat4low.Text = Roller4.getLowestNumber.ToString
+        stat4total.Text = Roller4.getScore.ToString
+        stat4mod.Text = Roller4.getModifer.ToString
+
+        stat5roll1.Text = Roller5.getRoll1.ToString
+        stat5roll2.Text = Roller5.getRoll2.ToString
+        stat5roll3.Text = Roller5.getRoll3.ToString
+        stat5roll4.Text = Roller5.getRoll4.ToString
+        stat5low.Text = Roller5.getLowestNumber.ToString
+        stat5total.Text = Roller5.getScore.ToString
+        stat5mod.Text = Roller5.getModifer.ToString
+
+        stat6roll1.Text = Roller6.getRoll1.ToString
+        stat6roll2.Text = Roller6.getRoll2.ToString
+        stat6roll3.Text = Roller6.getRoll3.ToString
+        stat6roll4.Text = Roller6.getRoll4.ToString
+        stat6low.Text = Roller6.getLowestNumber.ToString
+        stat6total.Text = Roller6.getScore.ToString
+        stat6mod.Text = Roller6.getModifer.ToString
+        'end chunk
+        TotalModCheck += Roller1.getModifer
+        TotalModCheck += Roller2.getModifer
+        TotalModCheck += Roller3.getModifer
+        TotalModCheck += Roller4.getModifer
+        TotalModCheck += Roller5.getModifer
+        TotalModCheck += Roller6.getModifer
+
+        TotalMod.Text = TotalModCheck.ToString
+        'MessageBox.Show("generate character function")
+
+
     End Sub
 
     Private Sub radFirstEdition_CheckedChanged(sender As Object, e As EventArgs) Handles radFirstEdition.CheckedChanged,
@@ -24,6 +101,7 @@
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         lblRNG1.Text = "Null"
         lblRNG2.Text = "Null"
         lblRNG3.Text = "Null"
@@ -31,4 +109,5 @@
         lblRNG5.Text = "Null"
         lblRNG6.Text = "Null"
     End Sub
+
 End Class
